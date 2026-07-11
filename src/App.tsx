eca916fb8e43394e4078a68dbad0086b4272cf99
mukhy <muutassim.mukhtar@mtn.com>
2026-07-11 17:45:36 +0100

    remove landing page from the product repo. marketing/docs now live in kern landing; console SPA routes to /app and links back to trykern.xyz.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ProtectedRoute } from "./ui/components/ProtectedRoute";
 import { AuthProvider } from "./ui/hooks/useAuth";
 import { ConsoleApp } from "./ui/pages/ConsoleApp";
-import { LandingPage } from "./ui/pages/LandingPage";
 import { LoginPage } from "./ui/pages/LoginPage";
 
 function App() {
@@ -10,7 +9,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<Navigate to="/app" replace />} />
           <Route path="/login" element={<LoginPage />} />
           <Route
             path="/app"
@@ -20,7 +19,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/app" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
